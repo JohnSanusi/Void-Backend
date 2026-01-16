890b5c7b0cb47e20a5c88feb0eb186304a6f7841
JohnSanusi <johnsanusi18@gmail.com>
2026-01-16 21:31:09 +0100

    import UsersModule in FeedModule

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { FeedService } from './feed.service';
 import { FeedController } from './feed.controller';
 import { Post, PostSchema } from './schemas/post.schema';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    UsersModule,
   ],
   controllers: [FeedController],
   providers: [FeedService],
